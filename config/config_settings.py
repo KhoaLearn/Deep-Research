390017cb0efd869590ab228eff3094ded30fa25f
KhoaLearn <khoale.maiu@gmail.com>
2024-08-08 22:57:56 +0700

    Craw VN30 Fixed

---
+++ config/config_settings.py
@@ -0,0 +1,10 @@
+import pathlib
+import pandas as pd
+import datetime
+import os
+
+VN30_TICKER = ['ACB', 'BID', 'BVH', 'CTG', 'FPT', 'GAS', 'GVR', 'HDB', 'HPG', 'KDH', 'MBB', 'MSN', 'MWG', 'NVL', 'PDR', 'PLX', 'POW', 'SAB', 'SSI', 'STB', 'TCB', 'TPB', 'VCB', 'VHM','VIB', 'VIC', 'VJC', 'VNM', 'VPB', 'VRE']
+
+config = {
+    "VN30_TICKER": VN30_TICKER
+}
